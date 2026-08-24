31675a4cb870952a7e72be62aa33e6ac7da5ffdc
kaysenplayz <k6072598@gmail.com>
2026-08-23 21:28:29 -0400

    Change activity type from 5 to 4 in bot config

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "Updating & Grinding",     // this is what people actually see
-        type: 5,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
